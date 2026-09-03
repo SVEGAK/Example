f6c81a764f5228b50e874738abacb97c7f79368a
Roman <roman141aleksandro2007@gmail.com>
2026-09-03 14:10:25 +0300

    Исправлена базовая задача

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
-std::cout << "Hello world!" << std::endl;
+int main() {
+	double a, h, s;
+	std::cin >> a >> h;
+	s = 0.5 * a * h;
+	std::cout << s << std::endl;
+	return 0;
+
+}
