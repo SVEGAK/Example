5dfae80e2dbb2b7f26a6e637302f6785d1998354
Новиков Роман <roman141aleksandro2007@gmail.com>
2026-09-05 23:41:39 +0300

    Исправлен интерфейс пользователя

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 int main() {
 	double a, h, s;
+	system("chcp 65001");system("cls");
+	std::cout << "Введите сторону и высоту треугольника через пробел: ";
 	std::cin >> a >> h;
 	s = 0.5 * a * h;
-	std::cout << s << std::endl;
+
+	std::cout << s << " -площадь для введенных стороны(" << a << ") и высоты(" << h << ")" << std::endl;
 	return 0;
 
 }
